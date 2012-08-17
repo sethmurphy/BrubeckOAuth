3e2b165d23f8f5f30cccf7ab31063395d03736b7
Seth Murphy <seth@sethmurphy.com>
2012-08-17 15:26:38 -0400

    Version bump for PyPi.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
  
 setup(name='brubeck-oauth',
-      version='0.1.4',
+      version='0.1.5',
       description='Brubeck OAuth module',
       author='Seth Murphy',
       author_email='seth@brooklyncode.com',
